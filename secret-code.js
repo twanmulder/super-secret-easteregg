ebce987cbce25278c0c73dc00e4f24c6c3d73385
TwanMulder <twan.mulder@gmail.com>
2020-03-25 22:06:13 +0100

    Worked on super secret code

---
+++ secret-code.js
@@ -0,0 +1,19 @@
+// Sorry this isn't es6, not everybody supports it yet /
+var b2U7vH90qC = [];
+var d6Gn2V7ml = document;
+var lA5tK3yT1m3 = Date.now();
+var eVt8b02nv9a = [(20).toString(36), (14).toString(36), (34).toString(36), (13).toString(36), (24).toString(36), (32).toString(36), (23).toString(36)].join("");
+
+d6Gn2V7ml.addEventListener(eVt8b02nv9a, function(e2h0R54) {
+  var cU8y2Px3 = Date.now();
+  if (cU8y2Px3 - lA5tK3yT1m3 > 1000) {
+    b2U7vH90qC = [];
+  }
+  lA5tK3yT1m3 = cU8y2Px3;
+
+  b2U7vH90qC.push(parseInt(e2h0R54.key, 36));
+  if (b2U7vH90qC.join("") === "29321023182812242421") {
+    var t1Mpu8cG5 = String.fromCodePoint(0x1f373).repeat(5);
+    console.log("%c" + t1Mpu8cG5 + "", "font-size: 1.5rem; background: #fec105; padding: 8px 12px; border-radius: 0.5rem;");
+  }
+});
